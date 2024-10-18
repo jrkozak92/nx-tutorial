d4d871b29cdca4153c1957a44b609f6e375d6fa7
jrkozak92 <jrkozak92@gmail.com>
2024-10-18 14:27:20 -0500

    initial commit, app running with multiple projects

--- apps/react-store-e2e/playwright.config.ts
+++ apps/react-store-e2e/playwright.config.ts
@@ -25,7 +25,7 @@ export default defineConfig({
   },
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'npx nx run react-store-:preview',
+    command: 'npx nx run react-store:preview',
     url: 'http://localhost:4300',
     reuseExistingServer: !process.env.CI,
     cwd: workspaceRoot,
--- apps/react-store/jest.config.ts
+++ apps/react-store/jest.config.ts
@@ -1,10 +1,10 @@
 export default {
-  displayName: 'react-store-',
+  displayName: 'react-store',
   preset: '../../jest.preset.js',
   transform: {
     '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
     '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/react/babel'] }],
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../coverage/apps/react-store-',
+  coverageDirectory: '../../coverage/apps/react-store',
 };
